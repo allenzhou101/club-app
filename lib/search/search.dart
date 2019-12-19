9303b41c7231c4470e03326fb93098110bb79291
Allen Zhou <46854522+allenzhou101@users.noreply.github.com>
2019-12-19 12:43:51 -0600

    Basic search bar made

--- lib/search/search.dart
+++ lib/search/search.dart
@@ -7,9 +7,98 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
-        home: Scaffold(
-            body: Text("Hello Brian K")
-        )
+        home: SearchWrapper()
     );
+  }
+}
+
+class SearchWrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+            appBar: AppBar(
+          title: Text("Search App"),
+          actions: <Widget>[
+            IconButton(icon: Icon(Icons.search), onPressed: () {
+              showSearch(context: context, delegate: DataSearch());
+            })
+          ],
+        ),
+    drawer: Drawer());
+  }
+}
+
+class DataSearch extends SearchDelegate<String> {
+
+  final cities = [
+    "Southlake",
+    "Austin",
+    "Houston",
+    "Dallas",
+    "Corpus Christi"
+  ];
+
+  final recentCities = [
+    "Southlake",
+    "Dallas"
+  ];
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    //actions for app bar
+    return [IconButton(icon: Icon(Icons.clear), onPressed: () {
+      query = "";
+    })];
+  }
+
+  @override
+  Widget buildLeading(BuildContext context) {
+    // leading icon on the left of the app bar
+    return IconButton(
+      icon: AnimatedIcon(
+        icon: AnimatedIcons.menu_arrow,
+        progress: transitionAnimation,
+      ),
+        onPressed: () {
+          close(context, null);
+        });
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    // show some result based on the selection
+    return Container(
+      height: 100.0,
+      width: 100.0,
+      child: Card(
+        color: Colors.red,
+        child: Center(
+          child: Text(query),
+        )
+      ),
+    );
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    // show when someone searches for something
+    final suggestionList = query.isEmpty
+        ? recentCities
+        : cities.where((p) => p.startsWith(query)).toList();
+
+    return ListView.builder(itemBuilder: (context, index) =>ListTile(
+      onTap: () {
+        showResults(context);
+      },
+        leading: Icon(Icons.location_city),
+        title: RichText(text: TextSpan(
+          text: suggestionList[index].substring(0, query.length),
+          style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+          children: [TextSpan(
+            text: suggestionList[index].substring(query.length),
+            style: TextStyle(color: Colors.grey)
+          )]
+        ))
+    ),
+    itemCount: suggestionList.length);
   }
 }
