781285d6627260db11d115b000fd79ed709d461c
Anthony Zhou <anthony.zhou03@gmail.com>
2020-01-02 13:58:40 -0600

    Update calendar.dart

--- lib/calendar/calendar.dart
+++ lib/calendar/calendar.dart
@@ -1,86 +1,49 @@
 import 'package:flutter/material.dart';
 
-class ExplorePage extends StatelessWidget {
+class CalendarPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListView(children: [
-      FeaturedCard(),
-      EventRow(category: "Popular Now"),
-      EventRow(category: "Technology"),
-      EventRow(category: "Sustainability"),
-      EventRow(category: "Culture")
-    ]);
-  }
-}
-
-class PlaceholderWidget extends StatelessWidget {
-  final Color color;
-
-  PlaceholderWidget(this.color);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      color: color,
-    );
-  }
-}
-
-//Scrollable row for each category of events
-class EventRow extends StatelessWidget {
-  final String category;
-
-  EventRow({this.category: ""});
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
       Padding(
-          padding: EdgeInsets.all(10),
-          child: Text(category,
-              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold))),
-      SingleChildScrollView(
-          scrollDirection: Axis.horizontal,
-          child: Row(mainAxisSize: MainAxisSize.min, children: [
-            EventCard(name: "Texas Grower's Farm Day", date: "Sat, Dec 7"),
-            SizedBox(width: 20),
-            EventCard(name: "Texas Grower's Farm Day", date: "Sat, Dec 7"),
-            SizedBox(width: 20),
-            EventCard(name: "Texas Grower's Farm Day", date: "Sat, Dec 7")
-          ]))
+        padding: const EdgeInsets.all(8.0),
+        child: Text("My Calendar", style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
+      ),
+      CalendarEvent(),
+      CalendarEvent(),
+      CalendarEvent(),
+      CalendarEvent(),
+      CalendarEvent(),
+      CalendarEvent(),
+      CalendarEvent(),
+      CalendarEvent(),
     ]);
   }
 }
 
-class EventCard extends StatelessWidget {
-  final String name, date;
-
-  EventCard({this.name: "", this.date: ""});
-
-  @override
-  Widget build(context) {
-    return SizedBox(
-        width: 200,
-        child: Column(children: [
-          Image.asset('assets/images/lanterns.jpg'),
-          ListTile(
-            title: Text(name),
-            subtitle: Text('Fri, Nov 29'),
-          )
-        ]));
-  }
-}
-
-class FeaturedCard extends StatelessWidget {
+class CalendarEvent extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Card(
-        child: Column(children: [
-          Image.asset('assets/images/canoe.jpg'),
-          ListTile(
-            title: Text('Concrete Canoe Club Meeting'),
-            subtitle: Text('Fri, Nov 29'),
-          )
+    return Padding(
+      padding: EdgeInsets.only(
+        top: 20,
+        left: 8
+      ),
+        child:Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text("8:30 AM", style: TextStyle(color: Colors.deepOrange)),
+          Text("Concrete Canoe Paddle Day", style: TextStyle(fontWeight: FontWeight.bold)),
+          Text("Concrete Canoe Team"),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Icon(Icons.location_on),
+              Text("Lady Bird Lake")
+            ]
+          ),
+          SizedBox(height: 15),
+          Divider()
         ]));
   }
 }
